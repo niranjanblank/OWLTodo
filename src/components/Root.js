1cf7e94b1b4c731deffcb226cb25fe7e0565981e
niranjan <niranjanshah474@gmail.com>
2023-09-11 10:12:13 +1000

    Delete functionality added

--- src/components/Root.js
+++ src/components/Root.js
@@ -9,6 +9,40 @@ export class Root extends Component {
     onMounted(()=> inputRef.el.focus())
   }
 
+  
+  nextId = 3;
+  tasks = useState([{
+    id: 1,
+    text: "Task 1",
+    isCompleted: false
+  },
+{
+  id:2,
+  text: "Task 2",
+  isCompleted: false
+}]);
+
+  addTask(ev){
+    if (ev.keyCode === 13){
+      const text = ev.target.value.trim()
+      ev.target.value = "";
+      if(text){
+        const newTask = {
+          id: this.nextId++,
+          text: text,
+          isCompleted: false
+        }
+        this.tasks.push(newTask)
+      }
+
+    }
+  }
+
+  deleteTask(task){
+    const index = this.tasks.findIndex(t => t.id == task.id)
+    this.tasks.splice(index,1)
+  }
+
 
   static template = xml`
     <div class="flex flex-col px-80 bg-zinc-900 min-h-screen text-white items-center">
@@ -18,28 +52,12 @@ export class Root extends Component {
     class="rounded-lg border-solid  border-green-400 w-1/2 p-4"
     placeholder="Enter a new task" t-on-keyup="addTask"/>
     <t t-foreach="tasks" t-as="task" t-key="task.id">
-        <Task task="task" />
+        <Task task="task" onDelete.bind="deleteTask"/>
     </t>
   
     </div>`;
 
-    nextId = 1;
-    tasks = useState([]);
 
-    addTask(ev){
-      if (ev.keyCode === 13){
-        const text = ev.target.value.trim()
-        ev.target.value = "";
-        if(text){
-          const newTask = {
-            id: this.nextId++,
-            text: text,
-            isCompleted: false
-          }
-          this.tasks.push(newTask)
-        }
-  
-      }
-    }
+
 
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,18 +1,26 @@
 import { Component, xml } from "@odoo/owl";
 
 export class Task extends Component {
-    static props = ['task']
+    static props = ['task', 'onDelete']
     
+    deleteTask(){
+        this.props.onDelete(this.props.task)
+    }
+
     static template = xml`
     <div class="rounded-xl my-2 py-2 px-5 w-1/2 bg-slate-800"
     t-att-class="props.task.isCompleted ? 'bg-green-400':''"
     >
         <t t-esc="props.task.text"/>
         <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="toggleTask" />
+        <span class="p-1 bg-red-500 rounded-lg"
+        t-on-click="deleteTask"
+        >Delete</span>
     </div>
     `
 
     toggleTask(){
         this.props.task.isCompleted = !this.props.task.isCompleted
     }
+
 }
